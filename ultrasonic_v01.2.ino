ae40dd094fb8d87af3143ab93ded3f2b9210af26
eyalbh <39707611+eyalbh@users.noreply.github.com>
2018-06-05 01:43:47 +0300

    Update ultrasonic_v01.2.ino

--- ultrasonic_v01.2.ino
+++ ultrasonic_v01.2.ino
@@ -16,6 +16,9 @@
 #define DISPLAY_CLK_PIN          8
 #define DISPLAY_DIO_PIN          9
 #define BUZZ_PIN                10
+#define LED_A_PIN               11
+#define LED_B_PIN               12
+#define LED_C_PIN               13
 #define MAX_DISTANCE           400
 #define DISTANCE_LEVEL_A        40
 #define DISTANCE_LEVEL_B        20
@@ -23,6 +26,9 @@
 #define SERIAL_COM_RATE     115200
 #define SONAR_SAMPLE_TIME      150 // [mSec]
 #define SONAR_NOF_SAMPLES        3
+#define BEEP_MODE_DISABLED       0
+#define BEEP_MODE_EN_FREQ        1
+#define BEEP_MODE_EN_CONST       2
 
 
 ///////////////////////////////////////////////////////
@@ -46,15 +52,29 @@ int dist_min;
 ///////////////////////////////////////////////////////
 //                       BEEP()                      //
 ///////////////////////////////////////////////////////
-void beep(int duration)
+void beep(int mode, int duration)
 {
     int t = 0;
-    for (t = 0; t < (duration/10); t++)
+    
+    if ( mode == BEEP_MODE_DISABLED )
     {
-        digitalWrite(BUZZ_PIN, HIGH);   // turn the BUZZ on (LOW is the voltage level)
-        delay(10);
         digitalWrite(BUZZ_PIN, LOW);   // turn the BUZZ off (LOW is the voltage level)
     }
+    
+    if ( mode == BEEP_MODE_EN_FREQ )
+    {
+        for (t = 0; t < (duration/10); t++)
+        {
+            digitalWrite(BUZZ_PIN, HIGH);   // turn the BUZZ on (LOW is the voltage level)
+            delay(10);
+            digitalWrite(BUZZ_PIN, LOW);   // turn the BUZZ off (LOW is the voltage level)
+        }        
+    }
+    
+    if ( mode == BEEP_MODE_EN_CONST )
+    {
+        digitalWrite(BUZZ_PIN, HIGH);   // turn the BUZZ on (LOW is the voltage level)
+    }    
 }
 
 
@@ -104,21 +124,41 @@ void loop() {
         display.showNumberDec(dist_min);
 
 
-        if (dist_min < 15)
+        if (dist_min < DISTANCE_LEVEL_C)
         {
-            beep(DISTANCE_LEVEL_C);
+            beep(BEEP_MODE_EN_CONST, 0);
+            digitalWrite(LED_A_PIN, HIGH);  
+            digitalWrite(LED_B_PIN, HIGH);  
+            digitalWrite(LED_C_PIN, HIGH);  
+            //beep(50);
             // digitalWrite(redLed, HIGH);   // turn the LED on (HIGH is the voltage level)
             //  digitalWrite(greenLed, HIGH);   // turn the LED on (HIGH is the voltage level)
         }
         else if (dist_min < DISTANCE_LEVEL_B)
         {
-            beep(100);
+            beep(BEEP_MODE_EN_FREQ, 50);
+            digitalWrite(LED_A_PIN, HIGH);  
+            digitalWrite(LED_B_PIN, HIGH);  
+            digitalWrite(LED_C_PIN, LOW);             
+            //beep(100);
             // digitalWrite(greenLed, HIGH);   // turn the LED on (HIGH is the voltage level)
         }
         else if (dist_min < DISTANCE_LEVEL_A)
         {
+            beep(BEEP_MODE_DISABLED, 0);    
+            digitalWrite(LED_A_PIN, HIGH);  
+            digitalWrite(LED_B_PIN, LOW);  
+            digitalWrite(LED_C_PIN, LOW);              
             // digitalWrite(yellowLed, HIGH);   // turn the LED on (HIGH is the voltage level)
             //   delay(100);
+        }
+        else
+        {
+            
+            beep(BEEP_MODE_DISABLED, 0);            
+            digitalWrite(LED_A_PIN, LOW);  
+            digitalWrite(LED_B_PIN, LOW);  
+            digitalWrite(LED_C_PIN, LOW);               
         }
     }
     
