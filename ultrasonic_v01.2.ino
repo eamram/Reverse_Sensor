d583386d47aa6e57370a0546162756f0181226a6
eyalbh <39707611+eyalbh@users.noreply.github.com>
2018-07-09 22:38:31 +0300

    Update ultrasonic_v01.2.ino

--- ultrasonic_v01.2.ino
+++ ultrasonic_v01.2.ino
@@ -1,9 +1,4 @@
-///////////////////////////////////////////////////////
-//                 HEADER FILES                      //
-///////////////////////////////////////////////////////
-#include <NewPing.h> //Load UltraSonic Library
-#include <Wire.h>
-#include <L3G.h>
+#include <NewPing.h>
 
 ///////////////////////////////////////////////////////
 //                 DEFINITIONS                       //
@@ -33,11 +28,6 @@
 #define SERIAL_COM_RATE     115200
 #define SONAR_SAMPLE_TIME       50 // [mSec]
 #define SONAR_NOF_SAMPLES        3
-#define BEEP_MODE_DISABLED       0
-#define BEEP_MODE_EN_FREQ        1
-#define BEEP_MODE_EN_CONST       2
-#define DISPLAY_OFF              0
-#define DISPLAY_ON               1
 #define MODE_LEVEL_DEF           5 
 #define MODE_LEVEL_A             4
 #define MODE_LEVEL_B             3    
@@ -54,7 +44,7 @@ NewPing sonar_b(TRIGGER_PIN_B, ECHO_PIN_B, MAX_DISTANCE);
 NewPing sonar_c(TRIGGER_PIN_C, ECHO_PIN_C, MAX_DISTANCE);
 NewPing sonar_d(TRIGGER_PIN_D, ECHO_PIN_D, MAX_DISTANCE);
 
-L3G gyro;
+//L3G gyro;
 int cycle_cnt;
 int sample_id;
 int dist_a[SONAR_NOF_SAMPLES];
@@ -71,27 +61,17 @@ int remainder;
 int upper_display_number;
 int lower_display_number;
 int mode_level;
-unsigned long StartTime;
-unsigned long CurTime;
-char display_data[] = {0x0, 0x0, 0x0, 0x0};
 
 ///////////////////////////////////////////////////////
 //                SETUP()                            //
 ///////////////////////////////////////////////////////
 void setup() 
 {
-    Serial.begin(SERIAL_COM_RATE);
-    Wire.begin();
-    if (!gyro.init())
-    {
-        Serial.println("Failed to autodetect gyro type!");
-    }
-    gyro.enableDefault();    
+    Serial.begin(SERIAL_COM_RATE); 
     pinMode(LED_A_PIN, OUTPUT);
     pinMode(LED_B_PIN, OUTPUT);
     pinMode(LED_C_PIN, OUTPUT);
-    pinMode(LED_D_PIN, OUTPUT);
-//    pinMode(LED_E_PIN, OUTPUT);    
+    pinMode(LED_D_PIN, OUTPUT);   
     cycle_cnt = 0;
 }
 
@@ -115,32 +95,8 @@ void loop() {
         dist_c_avg =  (dist_c[0]+dist_c[1]+dist_c[2])/3;
         dist_min = min(dist_a_avg, dist_b_avg);
         dist_min = min(dist_min, dist_c_avg);
-        dist_min = min(dist_min, dist_d_avg);        
-
-        // Print to serial port
-        Serial.print(dist_a_avg);
-        Serial.println(" a cm");
-        Serial.print(dist_b_avg);
-        Serial.println(" b cm");
-        Serial.print(dist_c_avg);
-        Serial.println(" c cm");
-        Serial.print(dist_d_avg);
-        Serial.println(" d cm");        
-        Serial.println("----------");
-        Serial.print(dist_min);
-        Serial.println(" cm min");
-        Serial.println("----------");
+        dist_min = min(dist_min, dist_d_avg);    
         
-        gyro.read();
-
-        Serial.print("G ");
-        Serial.print("X: ");
-        Serial.print((int)gyro.g.x);
-        Serial.print(" Y: ");
-        Serial.print((int)gyro.g.y);
-        Serial.print(" Z: ");
-        Serial.println((int)gyro.g.z);
-           
         // Calculate display number
         remainder = dist_min % DISPLAY_DIST_GAP;
         lower_display_number = dist_min - remainder;
@@ -156,7 +112,15 @@ void loop() {
         
         // Set level mode
         mode_level = MODE_LEVEL_DEF;
-        if ( display_number < DISTANCE_LEVEL_C )
+        if ( display_number < DISTANCE_LEVEL_E )
+        {
+            mode_level = MODE_LEVEL_E;
+        }        
+        else if ( display_number < DISTANCE_LEVEL_D )
+        {
+            mode_level = MODE_LEVEL_D;
+        }
+        else if ( display_number < DISTANCE_LEVEL_C )
         {
             mode_level = MODE_LEVEL_C;
         }
@@ -168,18 +132,26 @@ void loop() {
         {
             mode_level = MODE_LEVEL_A;            
         }        
-      
-        if ( gyro.g.x > 1000 ||  gyro.g.x < -1000 ||
-             gyro.g.y > 1000 ||  gyro.g.y < -1000 ||
-             gyro.g.z > 1000 ||  gyro.g.z < -1000 )
-        {
-            StartTime = millis();
-        }         
-        CurTime = millis();
-        if ((CurTime - StartTime) > 5000 )
-        {
-          mode_level = MODE_LEVEL_DEF;  
-        }
+
+        // Print to serial port
+        Serial.print(dist_a_avg);
+        Serial.println(" a cm");
+        Serial.print(dist_b_avg);
+        Serial.println(" b cm");
+        Serial.print(dist_c_avg);
+        Serial.println(" c cm");
+        Serial.print(dist_d_avg);
+        Serial.println(" d cm");        
+        Serial.println("----------");
+        Serial.print(dist_min);
+        Serial.println(" cm min");
+        Serial.println("----------");
+        Serial.print("Display Number: ");
+        Serial.println(display_number);
+        Serial.println("----------"); 
+        Serial.print("Mode: ");
+        Serial.println(mode_level);
+        Serial.println("----------");        
         
         // Set dispay, buzzer and LED behavior according to mode level   
         switch (mode_level)
@@ -188,48 +160,42 @@ void loop() {
                 digitalWrite(LED_A_PIN, HIGH);  
                 digitalWrite(LED_B_PIN, LOW);  
                 digitalWrite(LED_C_PIN, LOW);              
-                digitalWrite(LED_D_PIN, LOW);   
-   //             digitalWrite(LED_E_PIN, LOW);                   
+                digitalWrite(LED_D_PIN, LOW);                   
                 break;
                 
             case MODE_LEVEL_B:
                 digitalWrite(LED_A_PIN, HIGH);  
                 digitalWrite(LED_B_PIN, HIGH);  
                 digitalWrite(LED_C_PIN, LOW);
-                digitalWrite(LED_D_PIN, LOW);   
- //               digitalWrite(LED_E_PIN, LOW);                   
+                digitalWrite(LED_D_PIN, LOW);                    
                 break;
                 
             case MODE_LEVEL_C:
                 digitalWrite(LED_A_PIN, HIGH);  
                 digitalWrite(LED_B_PIN, HIGH);  
                 digitalWrite(LED_C_PIN, HIGH);
-                digitalWrite(LED_D_PIN, LOW);   
-//                digitalWrite(LED_E_PIN, LOW);                   
+                digitalWrite(LED_D_PIN, LOW);                     
                 break;
                 
             case MODE_LEVEL_D:
                 digitalWrite(LED_A_PIN, HIGH);  
                 digitalWrite(LED_B_PIN, HIGH);  
                 digitalWrite(LED_C_PIN, HIGH);
-                digitalWrite(LED_D_PIN, HIGH);   
-//                digitalWrite(LED_E_PIN, LOW);                   
+                digitalWrite(LED_D_PIN, HIGH);                    
                 break;                
                 
             case MODE_LEVEL_E:
                 digitalWrite(LED_A_PIN, HIGH);  
                 digitalWrite(LED_B_PIN, HIGH);  
                 digitalWrite(LED_C_PIN, HIGH);
-                digitalWrite(LED_D_PIN, HIGH);   
-//                digitalWrite(LED_E_PIN, HIGH);                   
+                digitalWrite(LED_D_PIN, HIGH);                     
                 break;                
                 
             default:        
                 digitalWrite(LED_A_PIN, LOW);  
                 digitalWrite(LED_B_PIN, LOW);  
                 digitalWrite(LED_C_PIN, LOW);
-                digitalWrite(LED_D_PIN, LOW);   
-//                digitalWrite(LED_E_PIN, LOW);                   
+                digitalWrite(LED_D_PIN, LOW);                      
                 break;
         }
     }
